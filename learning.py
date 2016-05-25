03bb5a923e2228f6759b7641f49c6ca4c445471a
tim9510019 <tim9510019@hotmail.com>
2016-05-25 11:28:27 +0800

    Random actions return range error
    random.randint(a, b)
    Return a random integer N such that a <= N <= b.

--- learning.py
+++ learning.py
@@ -47,7 +47,7 @@ def train_net(model, params):
 
         # Choose an action.
         if random.random() < epsilon or t < observe:
-            action = np.random.randint(0, 3)  # random
+            action = np.random.randint(0, 2)  # random
         else:
             # Get Q values for each action.
             qval = model.predict(state, batch_size=1)
